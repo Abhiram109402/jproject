dd495b0bf6afc5f1678a565a1cc1a20881e2ba11
Abhiram <akurathibalaji17@gmail.com>
2024-09-01 12:34:32 +0530

    My first file

---
+++ Palindrom.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Palindrom
+{
+public static void main(String [] args)
+{
+Scanner s=new Scanner(System.in);
+String str1,str2="";
+char ch;
+System.out.println("ENTER THE STRING");
+str1=s.nextLine();
+for(int i=0;i<str1.length();i++)
+{
+ch=str1.charAt(i);
+str2=ch+str2;
+}
+if(str1.equals(str2))
+System.out.println(" GIVEN STRING IS PALINDROM");
+else 
+System.out.println(" GIVEN STRING IS NOT PALINDROM");
+System.out.println("STR1=  "+str1);
+System.out.println("STR2=  "+str2);
+}
+}
